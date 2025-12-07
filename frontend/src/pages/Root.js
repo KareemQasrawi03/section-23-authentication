5f298ef1ce86e835f57c44b5dae6977badb55842
Kareem Qasrawi <kareemalqasrawi@gmail.com>
2025-12-07 11:06:45 +0300

    add setTimeout when use login in make use 1 h and automatic logout it use useForm for logout this is same btn logout but this solution still isnt perfect yet

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,27 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useRouteLoaderData,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    setTimeout(() => {
+      // this is same btn logout {form} make automatic logout if fench 1 h
+      submit(null, { action: "/logout", method: "post" });
+    }, 1 * 60 * 60 * 1000);// 1 houre
+  }, [token, submit]);
 
   return (
     <>
